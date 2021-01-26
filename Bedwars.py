db22ce4376654271cd557b17c040486668138e18
Rugg0064 <colony4ever@gmail.com>
2021-01-26 01:36:27 -0600

    Goodify

--- Bedwars.py
+++ Bedwars.py
@@ -2,68 +2,72 @@ from os import system
 from math import ceil
 import sys
 import os
-def contains(list1,string1):
-	string1 = string1.upper()
-	for i in list1:
-		if i==string1:
-			return True
-		else:
-			continue
-	return False
-def convert(l1):
-	converted = []
-	for i in l1:
-		for l in range(len(alias)):
-			if contains(alias[l],i):
-				converted.append(alias[l][0])
-				continue
-	return converted
 
-alias = [["WOOL","WO", "W"],
-["CLAY","C","CL"],
-["WOOD","WD"],
-["GLASS","G"],
-["ENDSTONE","E","END"]]
+def findBlockTable(alias):
+	for blockTable in blockPrices:
+		for tableAlias in blockTable[1]:
+			if(tableAlias == alias):
+				return blockTable
+	return None
+
+#Name, Aliases, Q, P, Iron/Gold/Emerald (0/1/2)
+blockPrices = [
+	["WOOL", ["WOOL","WO", "W"], 16, 4, "Iron"],
+	["CLAY", ["CLAY","C","CL"], 16, 12, "Iron"],
+	["WOOD", ["WOOD","WD"], 16, 4, "Gold"],
+	["GLASS", ["GLASS","G"], 4, 12, "Iron"],
+	["ENDSTONE", ["ENDSTONE","E","END"], 12, 24, "Iron"],
+	["OBSIDIAN", ["OBSIDIAN", "O"], 4, 4,"Emerald"]
+]
 
 blocks = []
 system('cls')
-while (usr:=input("Wool - W\nClay - C\nWood - Wd\nGlass - G\nEndstone - E\nBlock: ").upper()) !="":
-	system('cls')
-	blocks.append(usr)
-blocks = convert(blocks)
 
-def calcblocks(layer):
+usr = " "
+while (usr==" " or usr.upper() != ""):
+	for a in blockPrices:
+		for i, b in enumerate(a[1]):
+			print(b, end = "")
+			if(i <= len(a[1])-2):
+				print(" - ", end = "")
+		print()
+	usr = input("").upper()
+	if(block := findBlockTable(usr)):
+		blocks.append(block)
+	system('cls')
+
+def calcBlocks(layer):
 	if layer<0:
 		return 0
 	else:
-		#print("in this")
-		return (6+((layer-1)*4)) + calcblocks(layer-1)
+		return (6+((layer-1)*4)) + calcBlocks(layer-1)
 
-#Name, Q, P, Iron/Glass (1/0)
-blockprices = [["WOOL",16,4,0],
-["CLAY", 16, 12, 0],
-["WOOD", 16, 4, 1],
-["GLASS",4,12,0],
-["ENDSTONE",12,24,0]]
-print("N BLOCK     AMNT PRICE CURRENCY")
-totalpriceiron = 0
-totalpricegold = 0
-for i in range(len(blocks)-1,-1,-1):
-	output = ("%s" % str(i+1)) + " "*(3-len(str(i+1)))
-	output = output + "%s" % blocks[i] + " "*(10-len(blocks[i]))
-	amount = calcblocks(i+1)
-	output = output + str(amount) + " "*(4-len(str(amount)))
-	for j in range(len(blockprices)):
-		if blocks[i] == blockprices[j][0]:
-			cost = ceil(amount/blockprices[j][1])*blockprices[j][2]
-			output = output + str(cost) + (" "*(4-len(str(cost))))
-			if blockprices[j][3]==0:
-				output = output + "Iron"
-				totalpriceiron = totalpriceiron + cost
-			else:
-				output = output + "Gold"
-				totalpricegold = totalpricegold + cost
-	print(output)
-print("total ironx:", totalpriceiron)
-print("total gold:", totalpricegold)
+print("(Individual prices don't carry to a second layer)")
+blockCounts = [0] * len(blockPrices)
+for k, block in enumerate(blocks):
+	i = k + 1
+	blockCountIndex = None
+	for j, blockTable in enumerate(blockPrices):
+		if(block == blockTable):
+			blockCountIndex = j
+	amount = calcBlocks(i)
+	blockCounts[blockCountIndex] += amount
+	individualPrice = ceil(amount / block[2]) * block[3]
+	print("%s - %4d blocks - %s %s" % (block[0].ljust(8," "), amount, individualPrice, block[4]))
+
+
+totalPrices = {}
+print("Total prices (including carry over)")
+for i in range(0, len(blockCounts)):
+	table = blockPrices[i]
+	cost = ceil(blockCounts[i] / table[2]) * table[3]
+	if(cost > 0):
+		resource = blockPrices[i][4]
+		if(not resource in totalPrices.keys()):
+			totalPrices[resource] = cost
+		else:
+			totalPrices[resource] += cost
+	
+for key in totalPrices.keys():
+	print("%s - %d" % (key.ljust(7, " "), totalPrices[key]))
 input()
